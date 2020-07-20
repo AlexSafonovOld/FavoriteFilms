260543f6ff37457bb8eee2dfdda68558953c4a15
Alexey Safonov <student180591@gmail.com>
2020-07-20 16:45:22 +0300

    newModel

--- FavoriteFilms/Favorite Films/Model/FilmModel.swift
+++ FavoriteFilms/Favorite Films/Model/FilmModel.swift
@@ -7,9 +7,34 @@
 //
 
 import Foundation
+
+
+
 struct Film {
     let nameFilm: String
     let englishName: String
     let imageName: String
     let ganre: String
+    
+    
+    
+    init?(dict:[String: AnyObject]) {
+        guard let nameFilm = dict["nameFilm"] as? String,
+        let englishName = dict["englishName"] as? String,
+        let imageName = dict["image"] as? String,
+            let ganre = dict["ganre"] as? String else { return nil }
+        self.nameFilm = nameFilm
+        self.englishName = englishName
+        self.imageName = imageName
+        self.ganre = ganre
+    }
+    
+    //[
+    //  {
+    //    "nameFilm": "Безумный макс",
+    //    "englishName": "Mad Max",
+    //    "image": "https://fs.kinomania.ru/file/film/7/84/784b217f31e7568abfd3b4e870ccbee0.jpeg",
+    //    "ganre": "Боевик"
+    //  }
+    //]
 }
